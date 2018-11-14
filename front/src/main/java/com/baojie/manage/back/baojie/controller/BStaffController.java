ad43ea34ecadb7f443b2cd53dc68968a08d844ac
huangshuai1993 <798806653@qq.com>
2018-11-14 15:29:38 +0800

    修改

---
+++ front/src/main/java/com/baojie/manage/back/baojie/controller/BStaffController.java
@@ -0,0 +1,32 @@
+package com.baojie.manage.back.baojie.controller;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.baojie.manage.back.baojie.service.BStaffService;
+import com.baojie.manage.base.controller.BaseController;
+import com.baojie.manage.base.exception.BizException;
+
+@Controller
+@RequestMapping("bstaff")
+public class BStaffController extends BaseController {
+	@Autowired
+	private BStaffService staffService;
+
+	@RequestMapping("/updateTaskInfo")
+	@ResponseBody
+	public Map<String, Object> updateTaskInfo(HttpServletRequest request, @RequestParam(value = "id") Long id,
+			@RequestParam(value = "taskStatus") Integer taskStatus, Long storeId, Long empId, Integer removeStatus,
+			String removeText, Integer passStatus, String passText) throws BizException {
+		Map<String, Object> map = new HashMap<String, Object>();
+		return map;
+	}
+}
