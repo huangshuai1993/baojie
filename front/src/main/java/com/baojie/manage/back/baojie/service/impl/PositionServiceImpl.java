9edee5de3298f84cb2dcedb5118ff46434dbdf0c
huangshuai1993 <798806653@qq.com>
2018-11-23 17:24:18 +0800

    添加

---
+++ front/src/main/java/com/baojie/manage/back/baojie/service/impl/PositionServiceImpl.java
@@ -0,0 +1,10 @@
+package com.baojie.manage.back.baojie.service.impl;
+
+import org.springframework.stereotype.Service;
+
+import com.baojie.manage.back.baojie.service.PositionService;
+
+@Service("positionService")
+public class PositionServiceImpl implements PositionService {
+
+}
